c02d5b6f44301453aaadde3b60cd85f5c043154d
Abdelrahman Mostafa <158111829+AbdelrahmanMostafaHegazy@users.noreply.github.com>
2024-08-23 16:54:34 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,50 @@
+const orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        Shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        Shipping: 'Deliverd'
+    },
+
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        Shipping: 'Deliverd'
+    },
+
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+
+    {
+        productName: 'Lozenae Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        Shipping: 'Deliverd'
+    },
+]
